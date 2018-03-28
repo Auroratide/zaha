3f585959c91a9df17270dcd6a6e0c69d5946c92a
auroratide <tf.auroratide@gmail.com>
2018-03-27 21:30:52 -0500

    Reorganized tests

--- test/zaha.spec.js
+++ test/zaha.spec.js
@@ -4,45 +4,112 @@ const zaha = require('..');
 const is = zaha.is;
 
 describe('zaha' , () => {
-  let schema;
 
-  beforeEach(() => {
-    schema = {
-      key: is.string()
-    };
+  describe('basic types', () => {
+    let obj;
+
+    beforeEach(() => {
+      const Builder = zaha({
+        value: is('some value'),
+        string: is.string(),
+        number: is.number(),
+        int: is.int(),
+        boolean: is.boolean()
+      });
+
+      obj = new Builder().build();
+    });
+
+    it('should be the given value', () => {
+      expect(obj.value).to.exist;
+      expect(obj.value).to.equal('some value');
+    });
+
+    it('should be a string', () => {
+      expect(obj.string).to.exist;
+      expect(obj.string).to.be.a('string');
+    });
+
+    it('should be a number', () => {
+      expect(obj.number).to.exist;
+      expect(obj.number).to.be.a('number');
+    });
+
+    it('should be an integer', () => {
+      expect(obj.int).to.exist;
+      expect(obj.int).to.be.a('number');
+      expect(obj.int).to.equal(Math.floor(obj.int));
+    });
+
+    it('should be a boolean', () => {
+      expect(obj.boolean).to.exist;
+      expect(obj.boolean).to.be.a('boolean');
+    });
   });
 
-  it('should build an object', () => {
-    const Builder = zaha(schema);
-    const obj = new Builder().build();
-
-    expect(obj.key).to.exist;
-    expect(obj.key).to.be.a('string');
+  describe('objects', () => {
+    it('should accept other builders in the schema', () => {
+      const BuilderA = zaha({ a: is.string() });
+      const BuilderB = zaha({
+        b: new BuilderA()
+      });
+  
+      const obj = new BuilderB().build();
+  
+      expect(obj.b.a).to.exist;
+      expect(obj.b.a).to.be.a('string');
+    });
   });
 
-  it('should use the exact provided value', () => {
-    const Builder = zaha(schema);
-    const obj = new Builder()
-      .withKey('Exact Key')
-      .build();
+  describe('with', () => {
+    it('should use the exact provided value', () => {
+      const Builder = zaha({
+        key: is.string()
+      });
 
-    expect(obj.key).to.equal('Exact Key');
-  });
+      const obj = new Builder()
+        .withKey('Exact Key')
+        .build();
 
-  it('should use all provided values', () => {
-    schema.prop = is.string;
-    const Builder = zaha(schema);
-    const obj = new Builder()
-      .withKey('Exact Key')
-      .withProp('Another value')
-      .build();
+      expect(obj.key).to.equal('Exact Key');
+    });
 
-    expect(obj.key).to.equal('Exact Key');
-    expect(obj.prop).to.equal('Another value');
+    it('should use all provided values', () => {
+      const Builder = zaha({
+        keyA: is.string(),
+        keyB: is.number()
+      });
+
+      const obj = new Builder()
+        .withKeyA('Exact Key')
+        .withKeyB(55)
+        .build();
+  
+      expect(obj.keyA).to.equal('Exact Key');
+      expect(obj.keyB).to.equal(55);
+    });
+
+    it('should allow nested builders to be overridden', () => {
+      const BuilderA = zaha({ a: is.string() });
+      const BuilderB = zaha({
+        b: new BuilderA()
+      });
+  
+      const obj = new BuilderB()
+        .withB(new BuilderA()
+          .withA('a string')
+          .build())
+        .build();
+  
+      expect(obj.b.a).to.equal('a string');
+    });
   });
 
   it('should be extensible', () => {
-    const BaseBuilder = zaha(schema);
+    const BaseBuilder = zaha({
+      key: is.string()
+    });
+
     class Builder extends BaseBuilder {
       withJoy() {
         this.schema.key = is('Joy!');
@@ -57,30 +124,4 @@ describe('zaha' , () => {
     expect(obj.key).to.equal('Joy!');
   });
 
-  it('should accept other builders in the schema', () => {
-    const BuilderA = zaha(schema);
-    const BuilderB = zaha({
-      key: new BuilderA()
-    });
-
-    const obj = new BuilderB().build();
-
-    expect(obj.key.key).to.exist;
-    expect(obj.key.key).to.be.a('string');
-  });
-
-  it('should allow nested builders to be overridden', () => {
-    const BuilderA = zaha(schema);
-    const BuilderB = zaha({
-      key: new BuilderA()
-    });
-
-    const obj = new BuilderB()
-      .withKey(new BuilderA()
-        .withKey('a string')
-        .build())
-      .build();
-
-    expect(obj.key.key).to.equal('a string');
-  });
 });
